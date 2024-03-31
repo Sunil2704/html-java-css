994d9c7b527465ace36ef5b5d944153ffae92efa
sunil <pajapatisunil@gmail.com>
2024-03-31 17:59:34 +0530

    This is my First Commit

---
+++ JS/Programjs.js
@@ -0,0 +1,179 @@
+// console.log("Hello World")
+
+// var a
+// a=10
+// console.log(a)
+
+// var a
+// a=20
+// console.log(a)
+
+// // _______________________
+
+// let b
+// console.log(b)
+// b=10
+// console.log(b)
+// b=30
+// console.log(b)
+
+// // ---------
+//  const c=20
+//  console.log(c)
+//  console.log(c);
+
+
+// //  -----------
+
+// let x
+// log(x)
+// x=20
+// x=30
+// let y="hi"
+// log(x)
+// x="bye"
+// log(y)
+// log(x)
+
+
+// //declaration
+// let str="abc";
+// console.log(str);
+
+// //splitting the string value and converting it into array
+// let splitValue=str.split("");
+// console.log(splitValue);
+
+// //reversing the arry value 
+// let reversedValue=splitValue.reverse();
+// console.log(reversedValue);
+
+
+// //joining the reversed value and making it again string datatypr
+// let finalValue=reversedValue.join("");
+// console.log(finalValue);
+
+// let arr =[9,98,98,96,"Zero", "One"]
+// console.log(arr);
+
+// arr.pop()
+// console.log(arr);
+
+// arr.shift()
+// console.log(arr);
+
+// arr.push("Abc", 101)
+// console.log(arr);
+
+
+// arr.unshift(999,"xyz")
+// console.log(arr);
+
+
+// let arr =[9,98,98,96,"Zero", "One"]
+// arr.splice(1,2,"bingo","hello",45)
+// console.log(arr);
+
+// let sliced=arr.slice(2,3)
+// console.log(arr)
+// console.log(sliced);
+
+// let arr1= ["a","b","c","d"]
+// console.log(arr1);
+
+// let arr2= ["g","f","e"]
+// console.log(arr2);
+
+// let arr3= [arr1,arr2]
+// console.log(arr3[0][1]);
+
+
+// let arr4= arr1.concat(arr2)
+// console.log(arr4);
+
+// console.log(arr4[5]); //f
+
+
+// let person = {
+//     name: "Sunil",
+//     age: 27,
+//     skills:["sql","mt","css"],
+//     address: {
+//         vill: "pipirajatanpur",
+//         pincode: 274303
+//     }
+// }
+// console.log(person);
+// console.log(person.name);
+// console.log(person.age)
+
+// person.name = "Sunil Prasad"
+// console.log(person);
+
+// delete person.age
+
+// console.log(person);
+
+// person.salary = "500/day"
+// console.log(person);
+
+// console.log(person.address)
+// console.log(person.address.pincode);
+
+// console.log(x);
+// let x=function(){
+//     console.log("hi");
+// }
+// console.log(x)
+// x()
+
+// function y(){
+//     console.log("y");
+// }
+// console.log(y);
+// y()
+
+
+let arr1=["a","b","c"]
+let arr2=["d","e","f"]
+console.log(...arr1);
+let arr3=[arr1,arr2]
+console.log(arr3);
+
+
+
+// function x(a,...b)   //(...) only given in last parameter
+// {
+//     console.log(a);
+//     console.log(b);
+//     // console.log(c);
+// }
+// x(10,20,30,40,50,60)
+
+// let y=Math.max(10,20,-30,40,25)
+// console.log(y);
+
+
+// let x=Math.min(12,25,8,-85)
+// console.log(x);
+
+
+// let z=Math.floor(10.99)
+// console.log(z);
+
+
+// let r=Math.ceil(10.45)
+// console.log(r);
+
+// let s=Math.round(45.23)
+// console.log(s);
+
+
+// let arr=[14,25,45,665]
+// let maxValue=Math.max(...arr)
+// console.log(maxValue);
+
+
+
+
+
